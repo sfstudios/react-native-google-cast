7a73555373e3b06d5b048eca496466396f7a321f
Henning Hall <henning.hall@hotmail.com>
2020-06-24 14:45:06 +0200

    fix: hide menus when showing expanded controls (#6)

--- android/src/main/java/com/reactnative/googlecast/GoogleCastExpandedControlsActivity.java
+++ android/src/main/java/com/reactnative/googlecast/GoogleCastExpandedControlsActivity.java
@@ -1,14 +1,20 @@
 package com.reactnative.googlecast;
 
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.MediaRouteActionProvider;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
 
 public class GoogleCastExpandedControlsActivity extends ExpandedControllerActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
